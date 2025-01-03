c8c07be2f9b363d7117d3f0cb200feb8f14fbddb
André Caiado da Silva <a.caiadodasilva@xgeeks.com>
2025-01-03 16:54:58 +0000

    add spring boot project

---
+++ src/main/java/com/example/spring_boot_bpmn_camunda/SpringBootBpmnCamundaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_bpmn_camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBpmnCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBpmnCamundaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_bpmn_camunda/SpringBootBpmnCamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_bpmn_camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBpmnCamundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
